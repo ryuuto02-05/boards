b9e60ccd51dd4f44b18887070988e3a7b9ad67cb
ryuuto02-05 <ryuto.ch.02.05@gmail.com>
2025-02-18 18:42:06 +0900

    sai

--- siri/views.py
+++ siri/views.py
@@ -1,8 +1,11 @@
+import re
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.shortcuts import redirect, render
 from siri import models
+
+JAPANESE_PATTERN = re.compile(r"^[ぁ-んァ-ン一-龥ー]+$")
 
 
 def index(request):
@@ -16,24 +19,60 @@ def point(request):
     return render(request, "siri/index2.html", context)
 
 
+# def create(request):
+
+#     new_siri = models.Siri()
+#     new_siri.title = request.POST.get("title")
+#     new_siri.save()
+#     return HttpResponseRedirect(reverse("siri:index"))
+
+
 def create(request):
     title = request.POST.get("title")
+
+    if not JAPANESE_PATTERN.match(title):
+        return render(
+            request,
+            "siri/index.html",
+            {
+                "all_siri": models.Siri.objects.all(),
+                "error": "日本語のみ入力してください！",
+            },
+        )
+
     last_siri = models.Siri.objects.order_by("-id").first()
-    last_word = last_siri.title
-    last = last_word[-1]
-    head = title[0]
+    if last_siri is None:
+        if title != "しりとり":
+            return HttpResponseRedirect(reverse("siri:index"))
+    else:
+        # head == last
+        # new_siri = models.Siri()
+        # new_siri.title = title
+        # new_siri.finished = False
+        last_word = last_siri.title
+        last = last_word[-1]
+        head = title[0]
 
-    if head == last:
-        new_siri = models.Siri()
-        new_siri.title = title
-        new_siri.finished = False
+        if head != last:
+            return HttpResponseRedirect(reverse("siri:index"))
 
-        new_siri.save()
+    new_siri = models.Siri(title=title, finished=False)
+    new_siri.save()
+
     return HttpResponseRedirect(reverse("siri:index"))
+
+    # last_siri = models.Siri.objects.order_by("-id").first()
+    # last_word = last_siri.title
+
+    # last = last_word[-1]
+    # head = title[0]
+
+    # new_siri.save()
+    # return HttpResponseRedirect(reverse("siri:index"))
 
 
 def delete(request, siri_id):
     target_siri = models.Siri.objects.get(id=siri_id)
     target_siri.delete()
-    
+
     return HttpResponseRedirect(reverse("siri:index"))
